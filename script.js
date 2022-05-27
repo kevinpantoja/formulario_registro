c768a7bc0f04f37adae4a60cfabc7fc7d265e646
Kevin Pantoja <kevinpantoja146@gmail.com>
2022-05-27 12:02:05 -0500

    cambio de modalidad de mensajes

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ const validar_nombre = (texto)=>{
     return false;
 }
 const validar_celular = (texto)=>{
-    let exp_reg = /^(\+[1-9][0-9]{0,2}(\s)*)?[1-9][0-9]{8}$/i;
+    let exp_reg = /^[1-9][0-9]{8}$/i;
     if(exp_reg.test(texto)){
         return true;
     }
     return false;
 }
 const validar_correo = (texto)=>{
-    let exp_reg = new RegExp("^^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$","i");
+    let exp_reg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
     if(exp_reg.test(texto)){
         return true;
     }
@@ -46,75 +46,121 @@ const validar_direccion = (texto)=>{
 
 
 /*asignacion de eventos*/
-const analizar_input = ($elemento,funcion,mensaje = "")=>{
-    if(funcion($elemento.value)){
+const analizar_input = ($elemento,funcion)=>{
+    if(funcion($elemento.value.trim())){
         if($elemento.classList.contains("error_input")){
             $elemento.classList.remove("error_input")
-        }
-        $mensaje__error.innerText = mensaje;
-        if($mensaje__error.classList.contains("error__visible")){
-            $mensaje__error.classList.remove("error__visible");
         }
         return true;
     }else{
         if(!$elemento.classList.contains("error_input")){
             $elemento.classList.add("error_input")
         }
-        $mensaje__error.innerText = mensaje;
-        if(!$mensaje__error.classList.contains("error__visible")){
-            $mensaje__error.classList.add("error__visible");
-        }
         return false;
     }
 }
-$input_nombre.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"nombre inválido (mínimo 2 caracteres)");
+const mostrar_error = (mensaje)=>{
+    $mensaje__error.innerText = mensaje;
+    if(!$mensaje__error.classList.contains("error__visible")){
+        $mensaje__error.classList.add("error__visible");
+    }
+}
+const ocultar_error = ()=>{
+    if($mensaje__error.classList.contains("error__visible")){
+        $mensaje__error.classList.remove("error__visible");
+    }
+}
+
+
+/*funciones dentro de los eventos*/
+const evento_nombre = ($elemento)=>{
+    let solo_letras = /^[a-záéíóúáäëïöü\s]*$/
+    if(analizar_input($elemento,validar_nombre)){
+        ocultar_error();
+    }else{
+        if(!solo_letras.test($elemento.value)){
+            mostrar_error("El nombre solo debe tener letras");
+            return;
+        }
+        if($elemento.value.length < 2){
+            mostrar_error("El nombre debe tener como mínimo 2 caracteres");
+            return;
+        }
+    };
+}
+const evento_apellido = ($elemento)=>{
+    let solo_letras = /^[a-záéíóúáäëïöü\s]*$/
+    if(analizar_input($elemento,validar_nombre)){
+        ocultar_error();
+    }else{
+        if(!solo_letras.test($elemento.value)){
+            mostrar_error("El apellido solo debe tener letras");
+            return;
+        }
+        if($elemento.value.length < 2){
+            mostrar_error("El apellido debe tener como mínimo 2 caracteres");
+            return;
+        }
+    };
+}
+const evento_direccion = ($elemento)=>{
+    let solo_letras = /^[a-z0-9\.\-\s]*$/
+    if(analizar_input($elemento,validar_direccion)){
+        ocultar_error();
+    }else{
+        if(!solo_letras.test($elemento.value)){
+            mostrar_error("La dirección debe contener solo letras,números . y -");
+            return;
+        }
+        if($elemento.value.length < 8){
+            mostrar_error("La dirección debe tener 8 caracters como mínimo");
+            return;
+        }
+    };
+}
+const evento_correo = ($elemento)=>{
+    if(analizar_input($elemento,validar_correo)){
+        ocultar_error();
+    }else{
+        mostrar_error("Ingrese un e-mail válido");
+    };
+}
+const evento_celular = ($elemento)=>{
+    let solo_numeros = /^[0-9]*$/
+    if(analizar_input($elemento,validar_celular)){
+        ocultar_error();
+    }else{
+        if(!solo_numeros.test($elemento.value)){
+            mostrar_error("El celular solo debe contener números");
+            return;
+        }
+        if($elemento.value.length == 9){
+            mostrar_error("El celular debe tener 9 dígitos");
+            return;
+        }
+        if(solo_numeros.test($elemento.value) && $elemento.value[0] == "0"){
+            mostrar_error("El celular no debe comenzar en 0");
+            return;
+        }
+    };
+}
+
+
+inputs = [$input_nombre,$input_apaterno,$input_amaterno,$input_direccion,$input_telefono,$input_email];
+input_verificacion = [evento_nombre,evento_apellido,evento_apellido,evento_direccion,evento_celular,evento_correo];
+
+window.addEventListener("load", (m)=>{
+    for(let i = 0; i < inputs.length;i++){
+        inputs[i].addEventListener("input",(e)=>{
+            const $elemento = e.target;
+            input_verificacion[i]($elemento);
+        });
+        inputs[i].addEventListener("click",(e)=>{
+            const $elemento = e.target;
+            input_verificacion[i]($elemento);
+        });
+    }
 });
-$input_nombre.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"nombre inválido (mínimo 2 caracteres)");
-});
-$input_apaterno.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"solo apellido paterno");
-});
-$input_apaterno.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"solo apellido paterno");
-});
-$input_amaterno.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"solo apellido materno");
-});
-$input_amaterno.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_nombre,"solo apellido materno");
-});
-$input_telefono.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_celular,"celular solo permite números y +");
-})
-$input_telefono.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_celular,"celular solo permite números y +");
-})
-$input_email.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_correo,"ingrese un email válido");
-})
-$input_email.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_correo,"ingrese un email válido");
-})
-$input_direccion.addEventListener("input",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_direccion,"direccion (mínimo 8 caractéres)");
-})
-$input_direccion.addEventListener("click",(e)=>{
-    const $elemento = e.target;
-    analizar_input($elemento,validar_direccion,"direccion (mínimo 8 caractéres)");
-})
 
 
 /*validar antes de enviar*/
@@ -138,9 +184,7 @@ $enviar.addEventListener("click",(e)=>{
         [validar_nombre,validar_nombre,validar_nombre,validar_direccion,validar_correo,validar_celular]
     )){
         $form.submit();
-        console.log("listo para enviar");
     }else{
-        $mensaje__error.innerText = "no se puede enviar por datos inválidos";
-        console.log("aún hay elementos con errores");
+        mostrar_error("no se puede enviar por datos inválidos");
     }
 })
